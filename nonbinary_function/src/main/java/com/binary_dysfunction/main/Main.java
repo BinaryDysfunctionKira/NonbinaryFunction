a84eb21a67908fa54563378beca6d9758537cbe6
Kira <binarydysfunctionkira@gmail.com>
2026-09-22 21:18:23 +0200

    fix: new instance fix

--- nonbinary_function/src/main/java/com/binary_dysfunction/main/Main.java
+++ nonbinary_function/src/main/java/com/binary_dysfunction/main/Main.java
@@ -72,7 +72,7 @@ public class Main {
         try {
             String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
             String classpath = System.getProperty("java.class.path");
-            String mainClass = "com.binary_dysfunction.Main";
+            String mainClass = "com.binary_dysfunction.main.Main";
 
             List<String> command = new ArrayList<>();
             command.add(javaBin);
