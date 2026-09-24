c1b719b6f7b7f225594f50da9ab0fa27be652094
PBA3H25ASC <binarydysfunctionkira@gmail.com>
2026-09-24 08:30:40 +0200

    edit: home panel edited

--- nonbinary_function/src/main/java/com/binary_dysfunction/panels/HomePanel.java
+++ nonbinary_function/src/main/java/com/binary_dysfunction/panels/HomePanel.java
@@ -1,16 +1,24 @@
 package com.binary_dysfunction.panels;
 
 import java.awt.BorderLayout;
+import java.awt.Desktop;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 import com.binary_dysfunction.components.Colors;
+import com.binary_dysfunction.components.Component;
 import com.binary_dysfunction.components.UserCard;
+import com.binary_dysfunction.config.Config;
 import com.binary_dysfunction.frames.HomeFrame;
 import com.binary_dysfunction.main.Main;
 
@@ -37,19 +45,94 @@ public class HomePanel extends JPanel {
         headerPanel.add(new UserCard(hp), BorderLayout.EAST);
 
         
-        
+        JButton announcementButton = new JButton("Benachrichtigungen", Component.geticon("/notifications.png"));
+        announcementButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        announcementButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        announcementButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        announcementButton.addActionListener(e -> {
+            // hp.setNotificationsPanel();
+        });
 
-        JPanel mainContentContentPanel = new JPanel();
-        mainContentContentPanel.setLayout(new BoxLayout(mainContentContentPanel, BoxLayout.Y_AXIS));
+        JButton assembliesButton = new JButton("Benachrichtigungen", Component.geticon("/groups.png"));
+        assembliesButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        assembliesButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        assembliesButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        assembliesButton.addActionListener(e -> {
+            // hp.setAssembliesPanel();
+        });
+
+        JButton chatsButton = new JButton("Chats", Component.geticon("/chat-bubble.png"));
+        chatsButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        chatsButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        chatsButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        chatsButton.addActionListener(e -> {
+            hp.setChatPanel();
+        });
+
+        JButton calendarButton = new JButton("Kalender", Component.geticon("/calendar.png"));
+        calendarButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        calendarButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        calendarButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        calendarButton.addActionListener(e -> {
+            hp.setCalendarPanel();
+        });
+
+        JButton driveButton = new JButton("Drive Öffnen", Component.geticon("/drive-folder.png"));
+        driveButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        driveButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        driveButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        driveButton.addActionListener(e -> {
+            if (!Main.loggedInAccount.cloudActivated) {
+                JOptionPane.showMessageDialog(null, "Kein Zugriff auf den 'Personal-Vault'. Bitte fragen Sie ihre Administratoren auf Berechtigung an.", "Kein Zugriff auf die Cloud", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            try {
+                Desktop.getDesktop().open(new File(Main.serverPath + Config.ACCOUNTS_DIR + Main.loggedInAccount.username));
+            } catch (IOException e1) {
+                JOptionPane.showMessageDialog(null, "Kein Zugriff auf den 'Personal-Vault'. Bitte fragen Sie ihre Administratoren auf Berechtigung an.", "Kein Zugriff auf die Cloud", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        JButton eventModeButton = new JButton("Event-Funktionen", Component.geticon("/event.png"));
+        eventModeButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        eventModeButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        eventModeButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        eventModeButton.setVisible(false);
+        if (Main.loggedInAccount.assemblies.contains("Admin") || Main.loggedInAccount.assemblies.contains("Event")) eventModeButton.setVisible(true);
+        eventModeButton.addActionListener(e -> {
+            hp.setEventPanel();
+        });
+
+        JButton adminButton = new JButton("Admin Bereich", Component.geticon("/admin.png"));
+        adminButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 20,15));
+        adminButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        adminButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        adminButton.setVisible(false);
+        if (Main.loggedInAccount.assemblies.contains("Admin")) adminButton.setVisible(true);
+        adminButton.addActionListener(e -> {
+            hp.setAdminPanel();
+        });
+
+        JPanel mainContentContentPanel = new JPanel(new GridLayout(0, 3, 20, 20));
+        mainContentContentPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        // mainContentContentPanel.setLayout(new BoxLayout(mainContentContentPanel, BoxLayout.Y_AXIS));
         // mainContentContentPanel.setBackground(Colors.backgorundColorDarker);
+        mainContentContentPanel.add(announcementButton);
+        mainContentContentPanel.add(assembliesButton);
+        mainContentContentPanel.add(chatsButton);
+        mainContentContentPanel.add(calendarButton);
+        mainContentContentPanel.add(driveButton);
+        mainContentContentPanel.add(eventModeButton);
+        mainContentContentPanel.add(adminButton);
         
-        JScrollPane mainContentContentScrollPane = new JScrollPane(mainContentContentPanel);
-        mainContentContentScrollPane.setBorder(null);
-        mainContentContentScrollPane.setBackground(null);
+        // JScrollPane mainContentContentScrollPane = new JScrollPane(mainContentContentPanel);
+        // mainContentContentScrollPane.setBorder(null);
+        // mainContentContentScrollPane.setBackground(null);
+        // mainContentContentScrollPane.setVerticalScrollBar(null);
 
         JPanel mainContentPanel = new JPanel(new BorderLayout());
         mainContentPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-        mainContentPanel.add(mainContentContentScrollPane);
+        mainContentPanel.add(mainContentContentPanel);
 
         JPanel contentPanel = new JPanel(new BorderLayout());
         contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
